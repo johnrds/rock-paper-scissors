e140fe4759d49a22f276fd3f50537f0563b83ef1
johnrds <121064478+johnrds@users.noreply.github.com>
2023-01-16 16:23:38 -0300

    Created playJoKenPo function this function plays one round of rock, paper, scissors

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-let gameChoices = ['Rock', 'Paper', 'Scissors'];
+let gameChoices = ['rock', 'paper', 'scissors'];
 
-let getComputerChoice = () => gameChoices[Math.floor(Math.random()*3)];
+let getComputerChoice = () => gameChoices[Math.floor(Math.random() * 3)];
+
+function playJoKenPo(userChoice, computerChoice) {
+    userChoice = userChoice.toLowerCase();
+
+    if (userChoice === computerChoice) return 'Draw!'
+
+    if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            return 'You Lose! Paper beats Rock'
+        } else {
+            return 'You Won! Rock beats Scissors'
+        }
+    }
+
+    if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            return 'You Lose! Scissors beats Paper'
+        } else {
+            return 'You Won! Paper beats Rock'
+        }
+    }
+
+    if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            return 'You Lose! Rock beats Scissors'
+        } else {
+            return 'You Won! Scissors beats Paper'
+        }
+    }
+}
+
+const playerChoice = 'rock';
+const computerChoice = getComputerChoice();
+
+console.log(playJoKenPo(playerChoice, computerChoice));
